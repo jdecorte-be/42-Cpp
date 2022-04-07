c32e3c4732cd31f0481a31b14bcddd265d2f5e00
John Decorte <jdecorte@shi-r5-p5.s19.be>
2022-04-07 18:00:24 +0200

    update

--- cpp01/ex04/main.cpp
+++ cpp01/ex04/main.cpp
@@ -1,6 +1,15 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+void replace(std::ofstream &fd, std::string line, std::string str1, std::string str2)
+{
+    std::size_t pos = line.find(str1);
+    line.erase(pos, str1.length());
+    line.insert(pos, str2);
+    std::cout << line << std::endl;
+    fd.write(line.c_str(), line.length());
+    fd << std::endl;
+}
 
 int main(int ac, char **argv)
 {
@@ -8,15 +17,14 @@ int main(int ac, char **argv)
     if(ac != 4)
         return -1;
     std::string filename(argv[1]);
-    std::ifstream fd;
-    std::ifstream fd2;
-    fd.open(filename);
-    fd2.open(filename + ".replace");
+    std::string str1(argv[2]);
+    std::string str2(argv[3]);
+
+    std::ifstream fd(filename);
+    std::ofstream fd2(filename + ".replace");
+
     std::string line;
+    std::string toadd;
     for(int i = 0; getline(fd, line); i++)
-    {
-        for(int j = 0; line[j]; j++)
-            if(line[j] == argv[2])
-                line[j] = 
-    }
+        replace(fd2, line, str1, str2);
 }
