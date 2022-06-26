0d0eec48e0b9e29b45e4b647d61a12a309ff5a65
jdecorte42 <jdecorte42@Johns-MacBook-Air.local>
2022-06-26 15:47:52 +0200

    refchange

--- cpp01/ex02/main.cpp
+++ cpp01/ex02/main.cpp
@@ -7,7 +7,7 @@ int main()
     std::string &stringREF = str;
 
     std::cout << &str << std::endl;
-    std::cout << &stringPTR << std::endl;
+    std::cout << stringPTR << std::endl;
     std::cout << &stringREF << std::endl;
 
     std::cout << str << std::endl;
